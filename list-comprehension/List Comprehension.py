d724be9f148ed09e45b1ea7dcb64ac4c6d8152ff
sofib993 <sofoniasberhanek@gmail.com>
2026-07-21 20:26:13 +0300

    Refactor result generation using list comprehension

--- list-comprehension/List Comprehension.py
+++ list-comprehension/List Comprehension.py
@@ -3,11 +3,13 @@ if __name__ == '__main__':
     y = int(input())
     z = int(input())
     n = int(input())
-    
-    result=[]
-    for i in range(x+1):
-        for j in range(y+1):
-            for k in range(z+1):
-                if i + j + k != n:
-                    result.append([i,j,k])
+
+    result = [
+        [i, j, k]
+        for i in range(x + 1)
+        for j in range(y + 1)
+        for k in range(z + 1)
+        if i + j + k != n
+    ]
+
     print(result)
